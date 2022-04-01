7d4ce9f5f152a6d02f0fb08a1e46fdffdbc215db
gaetan <gaetancorin@hotmail.fr>
2022-04-01 15:29:03 +0200

    update creation bdd

--- site_django/filrouge/aquarium/admin.py
+++ site_django/filrouge/aquarium/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Aquarium
+from .models import Aquarium, Donnee_date, Donnee_releve, Donnee_note
 
 # Register your models here.
 admin.site.register(Aquarium)
+admin.site.register(Donnee_date)
+admin.site.register(Donnee_releve)
+admin.site.register(Donnee_note)
--- site_django/filrouge/aquarium/models.py
+++ site_django/filrouge/aquarium/models.py
@@ -1,23 +1,35 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Aquarium(models.Model):
     nom_aquarium = models.CharField(max_length=20)
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nom_aquarium
 
-class donnee_date(models.Model):
+class Donnee_date(models.Model):
     donnee_date = models.DateField()
+    aquarium = models.ForeignKey(Aquarium, on_delete=models.CASCADE)
 
     def __str__(self):
         return donnee_date
 
-class donnee(models.Model):
-    nom_donnee = models.CharField(max_length=15)
-    donnee = models.IntegerField()
+class Donnee_releve(models.Model):
+    nom_donnee_releve = models.CharField(max_length=15)
+    donnee_releve = models.IntegerField()
+    donnee_date = models.ForeignKey(Donnee_date, on_delete=models.CASCADE)
 
-class note_donnee(models.Model):
-    note_donnee = models.CharField(max_length=200)
+    def __str__(self):
+        return nom_donnee_releve
+
+class Donnee_note(models.Model):
+    donnee_note = models.CharField(max_length=200)
+    donnee_date = models.ForeignKey(Donnee_date, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return donnee_note
