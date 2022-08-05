8bb0f8e4ae5d6246695eff5b7d2ae07aa38bfee8
gaetancorin <gaetancorin@gmail.com>
2022-08-06 00:59:01 +0200

    réalisation page register_login

---
+++ SITE_DEVELOPPEMENT/site_mvc_php/views/header_login_register.php
@@ -0,0 +1,6 @@
+<header id="header_login_register">
+    <div id="div_logo">
+        <img id="logoAquaData" src="./img/logoAquaData_hd.png" alt="Aqua Data logo" >
+        <p id="title_logo">AquaData</p>
+    </div>
+</header>
---
+++ SITE_DEVELOPPEMENT/site_mvc_php/views/login_register.php
@@ -0,0 +1,14 @@
+<section>
+
+    <div id="div_poissonBallon">
+        <img id="poissonBallon" src="./img/poissonballon.png" alt="Aqua Data logo" >
+    </div>
+
+    <div id="div_register_login">
+        <div id="center_bouton">
+            <a class="bouton_login_register" id="login" href="#">Se connecter</a>
+            <a class="bouton_login_register" id="register" href="#">S'inscrire</a>
+        </div>
+    </div>
+    
+</section>
