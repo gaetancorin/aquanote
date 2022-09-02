945f1b3b3d692384410cfeae997e48f6929108c8
gaetancorin <gaetancorin@gmail.com>
2022-09-02 05:51:03 +0200

    mémoire partie pop u p améliorer et ajout de la fonctionnalité changenameaqua

--- AQUANOTE/src/controllers/change_name_aqua.php
+++ AQUANOTE/src/controllers/change_name_aqua.php
@@ -1,11 +1,9 @@
 <?php
 // controllers/change_name_aqua.php
-
 require_once('src/lib/database.php');
 require_once('src/models/aquarium.php');
 
 function changeNameAqua(array $input){
-
     if (!isset($_SESSION)){
         session_start();
     };
@@ -20,24 +18,24 @@ function changeNameAqua(array $input){
     //////////////////////////////////////////////////////////////:
     // change_name_aqua //  
 
-    // récupération du nom de l'aquarium connecté
-    // L'utilisateur ne peut changer que celui sur lequel il est connecté 
     $database = new Database();
     $aquariumRepository = new AquariumRepository();
     $aquariumRepository->set_database($database);
 
+    // vérification des informations $_POST (input)
+    if (!isset($input['new_name_aquarium']) || $input['new_name_aquarium'] === ''){
+        throw new Exception('Vous devez indiquez un nom pour renommer l\'aquarium');
+    }
+    $new_name_aquarium = $input['new_name_aquarium'];
+
+    // récupération du nom de l'aquarium connecté
+    // L'utilisateur ne peut changer de nom que sur l'aquarium sur lequel il est connecté 
     $aquarium_connected = $aquariumRepository->getAquariumById($id_aquarium_connected);
     if ($aquarium_connected === null){
         throw new Exception('Une erreur sur votre session est survenue lors du changement du nom de votre aquarium');
     }
 
-    // check des informations $_POST (input)
-    if (!isset($input['new_name_aquarium']) || $input['new_name_aquarium'] === ''){
-        throw new Exception('Vous devez indiquez un nom pour renommer l\'aquarium'.$aquarium_connected->get_name_aquarium());
-    }
-    $new_name_aquarium = $input['new_name_aquarium'];
-
-    // check que le nouveau nom est différent de l'ancien
+    // vérification que le nouveau nom est bien différent de l'ancien
     if($new_name_aquarium === $aquarium_connected->get_name_aquarium()){
         throw new Exception('Pour changer le nom de "'.$aquarium_connected->get_name_aquarium().'", vous devez indiquez un autre nom');
     }
@@ -48,6 +46,5 @@ function changeNameAqua(array $input){
     } catch(Exception){
         throw new Exception('Une erreur est survenue lors du changement du nom de votre aquarium');
     }
-
     header('Location: index.php?action=valuesInsertion');
 }
